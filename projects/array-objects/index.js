599467e6ba35d2d06a7c01e43d99635a33547d08
Ilya Pisligin <glkz.pisligin@gmail.com>
2021-04-30 16:43:21 +0300

    homework arrays-objects

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,51 +9,81 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
- Задание 2:
+   Задание 2:
+  
+   Напишите аналог встроенного метода map для работы с массивами
+   Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+  
+   Пример:
+     map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
+   */
+function map(array, fn) {
+  const newArray = [];
 
- Напишите аналог встроенного метода map для работы с массивами
- Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+  for (let i = 0; i < array.length; i++) {
+    const newArg = fn(array[i], i, array);
+    newArray.push(newArg);
+  }
 
- Пример:
-   map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
- */
-function map(array, fn) {}
-
-/*
- Задание 3:
-
- Напишите аналог встроенного метода reduce для работы с массивами
- Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
-
- Пример:
-   reduce([1, 2, 3], (all, current) => all + current) // 6
- */
-function reduce(array, fn, initial) {}
+  return newArray;
+}
 
 /*
- Задание 4:
+   Задание 3:
+  
+   Напишите аналог встроенного метода reduce для работы с массивами
+   Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
+  
+   Пример:
+     reduce([1, 2, 3], (all, current) => all + current) // 6
+   */
+function reduce(array, fn, initial) {
+  let all = initial ? fn(initial, array[0], 0, array) : array[0];
 
- Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+  for (let i = 1; i < array.length; i++) {
+    all = fn(all, array[i], i, array);
+  }
 
- Пример:
-   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
- */
-function upperProps(obj) {}
+  return all;
+}
 
 /*
- Задание 5 *:
+   Задание 4:
+  
+   Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+  
+   Пример:
+     upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+   */
+function upperProps(obj) {
+  const array = [];
 
- Функция принимает объект и должна вернуть Proxy для этого объекта
- Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+  for (const value in obj) {
+    const newValue = value.toUpperCase();
+    array.push(newValue);
+  }
 
- Пример:
-   const obj = createProxy({});
-   obj.foo = 2;
-   console.log(obj.foo); // 4
- */
+  return array;
+}
+
+/*
+   Задание 5 *:
+  
+   Функция принимает объект и должна вернуть Proxy для этого объекта
+   Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+  
+   Пример:
+     const obj = createProxy({});
+     obj.foo = 2;
+     console.log(obj.foo); // 4
+   */
 function createProxy(obj) {}
 
 export { forEach, map, reduce, upperProps, createProxy };
